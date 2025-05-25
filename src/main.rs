7838b9e684c59ad3809db7b2b507942977cc33a9
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-05-25 10:52:49 -0400

    Rotating ship

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use raylib::prelude::*;
+use std::cmp;
 
 const PI: f32 = 3.14159265359;
 const WINDOW_D: (i32, i32) = (640, 580);
@@ -43,7 +44,7 @@ struct Player {
     ammo: i32,
     velocity: f64,
     accel: f64,
-    displacement: [f32; 2]
+    displacement: Vector2
 }
 
 struct State {
@@ -65,11 +66,11 @@ fn main() {
         thread: thread,
         player: Player {
             lives: 3,
-            angle_r: PI,
+            angle_r: 0.0,
             ammo: 5,
             velocity: 0.0,
             accel: 0.0,
-            displacement: [0.0; 2]
+            displacement: Vector2::zero()
         },
         delta: 0.0,
         keys: [false; 31]
@@ -84,20 +85,21 @@ fn main_loop(state : &mut State) {
     while !state.rl_handle.window_should_close() {
         let cur_time = state.rl_handle.get_time();
         state.delta = cur_time - prev_time;
+        // state.player.angle_r += state.delta as f32;
 
         update_inputs(state);
         update_player(state);
 
-        let ship_lines = get_ship_lines(state);
+        let ship_lines = serialize_player(state);
 
         let mut d = state.rl_handle.begin_drawing(&state.thread);
         d.clear_background(Color::BLACK);
         d.draw_line_strip(&ship_lines, Color::WHITE);
 
+        println!("Angle: {}", state.player.angle_r);
+        println!("Accel: {}", state.player.accel);
+        println!("Vel: {}", state.player.velocity);
         prev_time = cur_time;
-
-        println!("Space: {}", state.keys[0]);
-        println!("Delta: {}", state.delta);
     }
 }
 
@@ -110,30 +112,50 @@ fn update_inputs(state : &mut State) {
 }
 
 fn update_player(state : &mut State) {
-    if state.keys[26] {
-        state.player.displacement[0] += 0.1;
+    // rotate at constant velocity?
+    // unless moving
+    if state.keys[26] { // right
+        state.player.angle_r += 0.0012;
     }
-    if state.keys[27] {
-        state.player.displacement[0] -= 0.1;
+    if state.keys[27] { // left
+        state.player.angle_r -= 0.0012;
     }
     if state.keys[28] {
-        state.player.displacement[1] -= 0.1;
+        if state.player.accel < 0.01 {
+            state.player.accel += 0.001;
+        }
+    } else if state.player.accel > 0.0 {
+        state.player.accel = (state.player.accel - 0.001).max(0.0);
     }
-    if state.keys[29] {
-        state.player.displacement[1] += 0.1;
-    }
+
+    state.player.velocity += state.player.accel * state.delta;
+
+    // how to find vector from angle?
+    // x = cos theta * hyp
+    // y = sin theta * hyp
+    // hyp = 1 cause unit vector
+    let theta: f32 = state.player.angle_r - (PI * 3.0/2.0);
+    let mut displacement_delta : Vector2 = Vector2::new(theta.cos(), theta.sin()); 
+    displacement_delta.scale(state.player.velocity as f32);
+
+    state.player.displacement += displacement_delta;
+    // find displacement vector
+    // (1) find unit direction vector
+    // (2) move along it by velocity
+    // (3) add to current displacement vector
 }
 
-fn get_ship_lines(state : &State) -> [Vector2; 4]{
+fn serialize_player(state : &State) -> [Vector2; 4]{
+    // center should be at (0, 0)
     let mut ship_lines: [Vector2; 4] = [Vector2::new(-0.35, 0.0),
                                         Vector2::new(0.0, 1.0),
                                         Vector2::new(0.35, 0.0),
                                         Vector2::new(-0.35, 0.0)];
     for i in &mut ship_lines {
+        i.rotate(state.player.angle_r);
         i.scale(25.0);
         *i += Vector2::new(640.0/2.0, 480.0/2.0);
-        *i += Vector2::new(state.player.displacement[0],
-                           state.player.displacement[1]);
+        *i += state.player.displacement;
     }
 
     ship_lines
